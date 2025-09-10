6eecb9486ff869214745da3ae9f64164251f7e33
plugado <jvoobmep@gmail.com>
2025-09-09 23:19:53 -0300

    esqueleto

--- encryption.py
+++ encryption.py
@@ -1,6 +1,9 @@
-def tobinary(texto: str, encoding: str = 'latin-1', separador: str = ' ') -> bytes:
+def to_bytes(texto: str, encoding: str = 'latin-1', separador: str = ' ') -> bytes:
     return texto.encode(encoding)
 
 
 def encrypt(word: bytes) -> bytes:
     return word
+
+def to_binary_stream(data: bytes) -> str:
+    return "".join(f"{byte:08b}" for byte in data)
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import interface
+import encryption
+import mlt3
 
 print("Vamos chamar a interface para pegar uma frase.")
 
-frase = interface.getSentence()
+frase_lista = interface.getSentence()
 
-# O programa principal continua a execução após a janela da interface ser fechada.
-if frase:
+frase_completa = "\n".join(frase_lista)
+
+if frase_completa.strip():
+
+    bytes_originais = encryption.to_bytes(frase_completa)
+    bytes_criptografados = encryption.encrypt(bytes_originais)
+
+    stream_binaria = encryption.to_binary_stream(bytes_criptografados)
+    
+    sinal_mlt3 = mlt3.mlt3_encode(stream_binaria)
+
     print("\nO programa principal recebeu as seguintes frases:")
     
-    print({frase})
+    print(f"Frases Recebidas: {frase_lista}")
+    print(f"Binário      : {encryption.to_binary_stream(bytes_originais)}")
+    print(f"Criptografado: {encryption.to_binary_stream(bytes_criptografados)}")
     
     print("\nProcessamento finalizado.")
 else:
